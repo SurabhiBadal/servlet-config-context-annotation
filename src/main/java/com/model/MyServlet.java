b94a36c05182155dc138ff503e14c3b16110a9a2
SurabhiBadal <s.badal2012@gmail.com>
2021-05-31 03:41:34 +0530

    Servlet Contect - Context Param

---
+++ src/main/java/com/model/MyServlet.java
@@ -0,0 +1,27 @@
+package com.model;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+	
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out=resp.getWriter();
+		
+		out.println("My Servlet");
+		
+		ServletContext ctx=getServletContext();
+		String str=ctx.getInitParameter("Name");
+		out.println("Name:"+str);
+		
+		
+	}
+
+}
